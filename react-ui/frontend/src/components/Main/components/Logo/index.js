0101220b230a52d37413b908c3fea20ae762d876
mehrdadkarami <mehrdad.allahkarami@syncier.com>
2020-03-24 10:43:21 +0100

    provided first react ui views with products and details

---
+++ react-ui/frontend/src/components/Main/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React, {PureComponent} from 'react';
+import './index.css';
+import productLogo from './product-logo-png.png';
+
+class Logo extends PureComponent {
+  render() {
+    return (
+        <div className="logo parent">
+          <img alt="product-logo" src={productLogo}/>
+        </div>
+    )
+  }
+}
+
+export default Logo;
