4b996644ece203a4d3688a126c0aff52dbd3c70d
mehrdad.karami <mehrdadallahkarami@gmail.com>
2022-10-27 20:27:48 +0200

    upgraded Spring boot + added Postgres TestContainer

--- cart-microservice/src/test/java/com/metao/book/cart/service/ShoppingCartServiceTest.java
+++ cart-microservice/src/test/java/com/metao/book/cart/service/ShoppingCartServiceTest.java
@@ -1,49 +1,12 @@
 package com.metao.book.cart.service;
 
-import com.metao.book.cart.domain.ShoppingCart;
-import com.metao.book.cart.domain.ShoppingCartKey;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ImportAutoConfiguration(classes = {ShoppingCartCartFactory.class})
 class ShoppingCartServiceTest {
 
-    @Autowired
-    private ShoppingCartService shoppingCartService;
 
-    @Test
-    void addProductToShoppingCart() {
-        shoppingCartService.addProductToShoppingCart(ConstantsTest.USER_ID, ConstantsTest.ASIN);
-        var productsInCart = shoppingCartService.getProductsInCartByUserId(ConstantsTest.USER_ID);
-        assertTrue(productsInCart.containsKey(ConstantsTest.USER_ID));
-        var shoppingList = List.of(ShoppingCart.createCart(new ShoppingCartKey(ConstantsTest.USER_ID, ConstantsTest.ASIN)));
-        assertThat(productsInCart
-                .get(ConstantsTest.USER_ID))
-                .isEqualTo(shoppingList)
-                .hasSize(1)
-                .contains(ShoppingCart.createCart(new ShoppingCartKey(ConstantsTest.USER_ID, ConstantsTest.ASIN)));
-    }
-
-    @Test
-    void getProductsInCart() {
-        shoppingCartService.getProductsInCartByUserId(ConstantsTest.USER_ID);
-    }
-
-    @Test
-    void removeProductFromCart() {
-    }
-
-    @Test
-    void createCart() {
-    }
-
-    @Test
-    void clearCart() {
-    }
 }
